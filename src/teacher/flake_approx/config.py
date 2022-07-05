0d52a40255563672f99a14a03a3d54d2b1a25aaf
Jonas Loos <33965649+JonasLoos@users.noreply.github.com>
2022-07-05 22:54:30 +0200

    cleanup

--- src/teacher/flake_approx/config.py
+++ src/teacher/flake_approx/config.py
@@ -10,7 +10,7 @@ other files should import the constants defined here
 MAP_NAME = '5x5_simple'
 
 
-# Number of trials (students run in parallel) to get a good average during mode evaluation
+# Number of trials (students trained per mode) to get a good average during mode evaluation
 # This might not incude the bandit policy, see `compare_teachers.run_bandits`
 # The paper used 10
 NUMBER_OF_TRIALS = 10
@@ -20,16 +20,3 @@ NUMBER_OF_TRIALS = 10
 # The paper used ['Trained', 'SR1', 'SR2', 'HR', 'Original', 'Bandit']
 # Custom modes: ['Halfway', 'Incremental']
 INTERVENTION_MODES = ['Halfway', 'Trained', 'Incremental']
-
-
-# INTERVENTIONS : "list[dict[str,Any]]" = [{
-#     'name': 'Trained',
-#     'label': '',
-#     'model': lambda: SingleSwitchPolicy.load(os.path.join(teacher_dir,'trained_teacher')),
-#     'teacher_env': partial(create_teacher_env, obs_from_training=True),
-# },{
-#     'name': 'Original',
-#     'label': '',
-#     'model': lambda: OpenLoopTeacher([0]),
-#     'teacher_env': partial(create_teacher_env, original=True),
-# }]
